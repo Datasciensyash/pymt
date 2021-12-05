31cf137208e0a837112e4d2454f536cab6d98bc7
Datasciensyash <gribulae@gmail.com>
2021-12-05 15:32:52 +0300

    Add parallel computation functions for 2D/3D tasks.

--- pymt/direct_task.py
+++ pymt/direct_task.py
@@ -2,23 +2,23 @@ import typing as tp
 import numpy as np
 from math import atan, pi, degrees
 from numpy import e as e_const
-import numba as nb
-from numba import jit
+
+from numba import jit, prange
 
 
 @jit(nopython=True)
 def direct_task_1d(
-    periods: np.ndarray,
-    layer_resistance: np.ndarray,
-    layer_power: np.ndarray,
+        periods: np.ndarray,
+        layer_resistance: np.ndarray,
+        layer_power: np.ndarray,
 ) -> tp.Tuple[np.ndarray, np.ndarray]:
     """
     Calculate one-dimensional direct task of MT.
 
     Args:
         periods (np.ndarray): Array of periods, e.g. [0.01, 0.005, ...]
-        layer_resistance (np.ndarray): electrical resistance by layer in Ohm * m (P)
-        layer_power (np.ndarray): Power of layers in m (H)
+        layer_resistance (np.ndarray): electrical resistance by layer in Ohm * m
+        layer_power (np.ndarray): Power of layers in m
 
     Returns:
         rho (np.ndarray): Rho field over model.
@@ -30,12 +30,11 @@ def direct_task_1d(
 
     # Vectorize as much as possible
     indexes = np.arange(num_layers - 1, 0, -1)
-    indexes_ = indexes - 1
-    k_array = np.sqrt(mu_zero_j / layer_resistance[indexes_])
-    a_array = np.sqrt(layer_resistance[indexes_] / layer_resistance[indexes])
+    k_array = np.sqrt(mu_zero_j / layer_resistance[indexes - 1])
+    a_array = np.sqrt(layer_resistance[indexes - 1] / layer_resistance[indexes])
 
     rho_t, phi_t = np.empty(periods.shape[0]), np.empty(periods.shape[0])
-    for i in nb.prange(periods.shape[0]):
+    for i in range(periods.shape[0]):
         r = 1.0
         omega = (2 * pi / periods[i]) ** 0.5
         for m in range(num_layers - 1, 0, -1):
@@ -48,5 +47,69 @@ def direct_task_1d(
 
     # Additional vectorization out of loop
     phi_t = phi_t - 45
+
+    return rho_t, phi_t
+
+
+@jit(nopython=True, parallel=True)
+def direct_task_2d(
+        periods: np.ndarray,
+        layer_resistance: np.ndarray,
+        layer_power: np.ndarray
+):
+    """
+    Calculate two-dimensional direct task of MT.
+
+    Args:
+        periods (np.ndarray):
+            Array of periods, e.g. [0.01, 0.005, ...]
+        layer_resistance (np.ndarray):
+            Electrical resistance by layer in Ohm * m, with shape (Width, LayerNum + 1)
+        layer_power (np.ndarray):
+            Power of layers in m, with shape (Width, LayerNum)
+
+    Returns:
+        rho (np.ndarray): Rho field over model with shape (Width, PeriodNum)
+        phi (np.ndarray): Phi field over model with shape (Width, PeriodNum)
+    """
+    rho_t = np.empty((layer_resistance.shape[0], periods.shape[0]))
+    phi_t = np.empty((layer_resistance.shape[0], periods.shape[0]))
+
+    for i in prange(layer_resistance.shape[0]):
+        rho, phi = direct_task_1d(periods, layer_resistance[i, :], layer_power[i, :])
+        rho_t[i, :] = rho
+        phi_t[i, :] = phi
+
+    return rho_t, phi_t
+
+
+@jit(nopython=True, parallel=True)
+def direct_task_3d(
+        periods: np.ndarray,
+        layer_resistance: np.ndarray,
+        layer_power: np.ndarray
+):
+    """
+    Calculate two-dimensional direct task of MT.
+
+    Args:
+        periods (np.ndarray):
+            Array of periods, e.g. [0.01, 0.005, ...]
+        layer_resistance (np.ndarray):
+            Electrical resistance by layer in Ohm * m, with shape (WidthX, WidthY, LayerNum + 1)
+        layer_power (np.ndarray):
+            Power of layers in m, with shape (WidthX, WidthY, LayerNum)
+
+    Returns:
+        rho (np.ndarray): Rho field over model with shape (WidthX, WidthY, PeriodNum)
+        phi (np.ndarray): Phi field over model with shape (WidthX, WidthY, PeriodNum)
+    """
+    rho_t = np.empty((layer_resistance.shape[0], layer_resistance.shape[1], periods.shape[0]))
+    phi_t = np.empty((layer_resistance.shape[0], layer_resistance.shape[1], periods.shape[0]))
+
+    for i in prange(layer_resistance.shape[0]):
+        rho, phi = direct_task_2d(periods, layer_resistance[i, :, :], layer_power[i, :, :])
+        rho_t[i, :, :] = rho
+        phi_t[i, :, :] = phi
 
     return rho_t, phi_t
